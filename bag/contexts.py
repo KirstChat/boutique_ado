8726e333162749d3938a821c45b4b33fc9ec8669
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-02-27 15:35:33 +0000

    update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,9 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
-""" 
+"""
 Return a dictionary called context
 Context processor - purpose is to make this dictionary available
 To all templates accross the entire app
@@ -13,6 +15,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(
