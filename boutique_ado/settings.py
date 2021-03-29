2970bb669a01ff9eb7b83848ef8d04cd116fb511
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-03-29 21:33:36 +0100

    deploy to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 from os import path
 if path.exists('env.py'):
@@ -130,7 +131,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} 
 
 
 # Password validation
